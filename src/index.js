7241ccbc378a84049cf6b8e2418609b0cef1a2d1
Dimadzu <Dimadzu@i.ua>
2021-03-09 00:33:43 +0300

    feat: add function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let strArr= String(n).split('');
+    let newStrArr=[];
+   for(let n = strArr.length; n>=0; n--){
+    if(strArr[n]!=="-"){
+       newStrArr.push(strArr[n]);}
+   }
+   let reverseNumber= Number(newStrArr.join(""));
+   return reverseNumber;
+    
 }
